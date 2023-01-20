0f5b5f50d53c88be95b1bfdeefae0bafa5083bb2
SainasFede <sainasfede@gmail.com>
2023-01-20 16:58:39 +0100

    fixed show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
     <div class="container">
-        <h1>Qui si mostra</h1>
+        <div class="d-flex justify-content-between my-5">
+            <h1 class="d-inline">Mostra il progetto</h1>
+            <div>
+                <a class="btn btn-success" href="{{route('admin.projects.index')}}">Torna all'elenco Progetti</a>
+            </div>
+        </div>
         <div class="card" style="width: 18rem;">
             <img src="{{ $project->cover_image ? asset('storage/' . $project->cover_image) : 'https://img.freepik.com/free-vector/illustration-data-folder-icon_53876-6329.jpg?w=2000'}}" class="card-img-top" alt="{{$project->name}}">
             <div class="card-body">
